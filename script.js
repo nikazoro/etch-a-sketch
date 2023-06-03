dd50057e364b017bd29096df2b79164e41470153
Suraj Singh Rana <officialsurajra2@gmail.com>
2023-06-03 12:57:04 +0530

    changed touchstart to touchmove to fix mobile issues

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function createDiv(width) {
         div.style.width = "100%"
         div.style.height = "100%"
         div.addEventListener("mouseover", () => drawingHover(div))
-        div.addEventListener("touchstart", () => drawingHover(div))
+        div.addEventListener("touchmove", () => drawingHover(div))
         drawingBox.appendChild(div)
     }
 }
